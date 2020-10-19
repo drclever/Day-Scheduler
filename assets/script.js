98d630c2de133c8ebb47aa81efd5ab65ae79babd
Donna Clever <drc0625@aol.com>
2020-10-19 12:43:29 -0400

    Making LocalStoarge access more efficient by using loop and jquery.

--- assets/script.js
+++ assets/script.js
@@ -3,84 +3,25 @@ $(document).ready(function() {
     //Display today's date in the jumbotron area of the page using moment.js
     const todayDate = document.querySelector("#currentDay");
     todayDate.textContent = moment().format("dddd, MMMM D, YYYY");
+    currentH = moment().format("H");
 
-/*     let inputTextarea9 = document.querySelector(".entry-9");
-    let clickButton9 = document.querySelector(".button9");
-
-    //Retrieve local storage
-    inputTextarea9.value = localStorage.getItem("text9");
-
-    //when the Save button is clicked, store the input of the textarea
-    clickButton9.addEventListener("click", updateText1);
-    //sets the input of text area into local storage
-    function updateText1() {
-        localStorage.setItem("text9", inputTextarea9.value);
-    }; */
+    //Load LocalStorage
+    var keys = Object.keys(localStorage);
+    for (let i =0; i < localStorage.length; i++) {
+        let value = localStorage.getItem(keys[i]);
+        let extractNum = keys[i].slice(4);
+        $(".entry-" + extractNum).text(value);
+    }
 
     //Use jquery to save textarea input to local storage.  Using military time for numbers. 
     //Start with 9am to 5pm in military time.
-    $(".button9").on("click", function () {
-        event.preventDefault();
-        let newText = $(".entry-9").val();
-        localStorage.setItem("text9", newText);
+    $(".saveBtn").on("click", function (event) {
+        var value1 = this.id;
+        var extractNum1 = value1.slice(6)
+        let newText = $(".entry-" + extractNum1).val();
+        localStorage.setItem("text" + extractNum1, newText);
     });
-    $(".entry-9").val(localStorage.getItem("text9"));
 
-    $(".button10").on("click", function () {
-        event.preventDefault();
-        let newText = $(".entry-10").val();
-        localStorage.setItem("text10", newText);
-    });
-    $(".entry-10").val(localStorage.getItem("text10"));
-
-    $(".button11").on("click", function () {
-        event.preventDefault();
-        let newText = $(".entry-11").val();
-        localStorage.setItem("text11", newText);
-    });
-    $(".entry-11").val(localStorage.getItem("text11"));
-    
-    $(".button12").on("click", function () {
-        event.preventDefault();
-        let newText = $(".entry-12").val();
-        localStorage.setItem("text12", newText);
-    });
-    $(".entry-12").val(localStorage.getItem("text12"));
-
-    $(".button13").on("click", function () {
-        event.preventDefault();
-        let newText = $(".entry-13").val();
-        localStorage.setItem("text13", newText);
-    });
-    $(".entry-13").val(localStorage.getItem("text13"));
-
-    $(".button14").on("click", function () {
-        event.preventDefault();
-        let newText = $(".entry-14").val();
-        localStorage.setItem("text14", newText);
-    });
-    $(".entry-14").val(localStorage.getItem("text14"));
-
-    $(".button15").on("click", function () {
-        event.preventDefault();
-        let newText = $(".entry-15").val();
-        localStorage.setItem("text15", newText);
-    });
-    $(".entry-15").val(localStorage.getItem("text15"));
-    
-    $(".button16").on("click", function () {
-        event.preventDefault();
-        let newText = $(".entry-16").val();
-        localStorage.setItem("text16", newText);
-    });
-    $(".entry-16").val(localStorage.getItem("text16"));
-
-    $(".button17").on("click", function () {
-        event.preventDefault();
-        let newText = $(".entry-17").val();
-        localStorage.setItem("text17", newText);
-    });
-    $(".entry-17").val(localStorage.getItem("text17"));
 
     // Determine Styling of each textarea box
     var currentHour= new Date().getHours();
